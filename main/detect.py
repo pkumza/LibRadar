a555433276c440a0a6c44b22d2ac7e07ac1755be
Zachary <pkumza@163.com>
2015-10-24 17:30:59 +0800

    把那些函数放到上面去。 把（未标注的库）的sp/csp/cp 改为 pn/cpn/p 以便在同一张表格中显示。

--- main/detect.py
+++ main/detect.py
@@ -66,231 +66,6 @@ class Detector:
 
         self.load_data()
 
-    def get_smali(self, path):
-        """
-        Convert APK into Smali file.
-        :param path:
-        :return: decoded files' path
-        """
-        self.time_decode.start()
-        cmd = self.project_path + "/" + "../tool/apktool decode %s -o " % path + self.project_path + "/" + \
-            "../decoded/%s" % os.path.basename(path)
-        subprocess.call(cmd, shell=True)
-        self.time_decode.end()
-        return self.project_path + '/../decoded/%s' % os.path.basename(path)
-
-    def load_data(self):
-        # - Loading Data
-
-        self.time_load.start()
-        dep_address = self.project_path + "/" + "../data/tgst5.dat"
-        dict_address = self.project_path + "/" + "../permission/tagged_dict.txt"
-        dep_file = open(dep_address, 'r')
-        dict_file = open(dict_address, 'r')
-
-        # -- Loading API Dict
-        for line in dict_file:
-            # print line
-            u = json.loads(line)
-            if 'p' in u:
-                self.api_dict[u['k']] = {'v': u['v'], 'p': u['p']}
-            else:
-                self.api_dict[u['k']] = {'v': u['v'], 'p': []}
-
-        # -- Loading Hashed Libs
-
-        for line in dep_file:
-            # print line
-            u = json.loads(line)
-            """
-            {"dn": 311,                         Repetitions
-             "lib": "pollfish",                 Library
-              "sp": "com/pollfish/f/a",         Simplified Path
-               "bh": 32370,                     B_Hash
-                "btc": 40,                      B_Total_Call
-                 "btn": 12,                     B_Total_Number
-                  "pn": "com/pollfish"}         Package Name
-            """
-            if "pn" in u:
-                components = u['lib'].split(';')
-                assert len(components) == 3
-                lib_type = components[0]
-                if lib_type in self.library_type:
-                    lib_type = self.library_type[lib_type]
-                eng_lib = components[1]        # English Library
-                ch_des = components[2]         # Web Link
-                self.libs_feature.append(
-                    (u['bh'], u['btn'], u['btc'], u['sp'], eng_lib, u['pn'], u['dn'], ch_des, lib_type)
-                )
-            else:
-                self.libs_feature.append((u['bh'],  u['btn'], u['btc'], u['sp'], u['lib'], "", u['dn'], "", ""))
-
-        self.time_load.end()
-
-    def get_hash(self, apk_path):
-        """
-        Convert APK into Smali file.
-        :param path:
-        :return: The path of apk with libs removed.
-        """
-
-        self.time_extract.start()
-
-        # - All Over
-        # print apk_path+'/smali'
-        if os.path.exists(apk_path+'/smali'):
-            os.chdir(apk_path+'/smali')
-            self.all_over(apk_path, '')
-            # os.chdir(apk_path)
-
-        # Print Res
-        # print packages_feature
-        '''
-        if DEBUG:
-            for p in packages_feature:
-                print p
-        '''
-        cur_app_libs = []
-        cur_app_routes = {}
-        path_and_permission = {}
-
-        number_of_tagged_libs = len(self.libs_feature)
-        self.time_extract.end()
-        self.time_compare.start()
-
-        def compare_d(a, b):
-            if a[1] < b[1]:
-                return -1
-            elif a[1] > b[1]:
-                return 1
-            else:
-                if a[2] < b[2]:
-                    return -2
-                elif a[2] > b[2]:
-                    return 2
-                else:
-                    if a[0] < b[0]:
-                        return -3
-                    elif a[0] > b[0]:
-                        return 3
-                    else:
-                        return 0
-
-        def find_feature(package, start, end):
-            if start >= end:
-                return None
-            mid = (start + end) / 2
-            """
-                packages_feature.append((bh, len(this_dict), this_call_num, '/'.join(parts), this_permission))
-            """
-            if compare_d(package, self.libs_feature[mid]) == 0:
-                if self.libs_feature[mid][4] != "" and self.libs_feature[mid][4] != "Nope":
-                    cur_app_libs.append({
-                        "bh": self.libs_feature[mid][0],
-                        "btn": self.libs_feature[mid][1],
-                        "btc": self.libs_feature[mid][2],
-                        "sp": self.libs_feature[mid][3],
-                        "lib": self.libs_feature[mid][4],
-                        "pn": self.libs_feature[mid][5],
-                        "dn": self.libs_feature[mid][6],
-                        "ch": self.libs_feature[mid][7],         # Web Link
-                        "tp": self.libs_feature[mid][8],
-                        "csp": package[3],                  # Current S_path
-                    })
-                elif self.libs_feature[mid][4] == "":
-                    cur_app_routes[self.libs_feature[mid][3]] = {
-                        "sp": self.libs_feature[mid][3],
-                        "csp": package[3],
-                        "cp": package[4],
-                        "dn": self.libs_feature[mid][6]
-                    }
-            elif compare_d(package, self.libs_feature[mid]) < 0:
-                return find_feature(package, mid + 1, end)
-            else:
-                return find_feature(package, start, mid)
-
-        if DEBUG_ON:
-            print "--Packages--"
-
-        def find_features(package):
-            if package[3] != "":
-                if len(package[4]) == 0:
-                    if DEBUG_ON:
-                        print package[3]
-                    path_and_permission[package[3]] = []
-                else:
-                    if DEBUG_ON:
-                        print package[3]+' -- Permission: '+str(package[4])
-                    path_and_permission[package[3]] = package[4]
-            find_feature(package, 0, number_of_tagged_libs)
-
-        for pack in self.packages_feature:
-            if DEBUG_PATH:
-                print ('PACK' + str(pack))
-            find_features(pack)
-        if DEBUG_ON:
-            print "PATH and Permission:"
-            for k in path_and_permission:
-                print k + str(path_and_permission[k])
-        print "--Splitter--"
-        final_libs_dict = {}
-        for i in cur_app_libs:
-            # 先找PN
-            # 然后切分sp。找到对应的path
-            # 然后把对应的Permission找出来加进来
-            if i['pn'] in final_libs_dict:
-                # 找到同样package name的小包中，重复次数最多的，作为总的重复次数
-                # 本来想找到大包的重复次数作为重复次数，但是大包的重复次数甚至很容易直接是0
-                # 并不适合。
-                if final_libs_dict[i['pn']]['dn'] < i['dn']:
-                    final_libs_dict[i['pn']]['dn'] = i['dn']
-            pn_number = len(i['pn'].split('/'))
-            cpn = '/'.join(i['csp'].split('/')[0:pn_number])
-            cpn += '/'
-            i['cpn'] = cpn
-            i['p'] = path_and_permission[cpn]
-            final_libs_dict[i['pn']] = i
-            #print str(i) + ','
-        final_libs_list = []
-        for i in final_libs_dict:
-            final_libs_list.append(final_libs_dict[i])
-        print json.dumps(final_libs_list)
-        print "--Splitter--"
-        final_routes_list = []
-        for i in cur_app_routes:
-            final_routes_list.append(cur_app_routes[i])
-        print json.dumps(final_routes_list)
-        print "--Splitter--"
-        self.time_compare.end()
-
-        # To String
-        # print "--Time-Consuming--"
-        self.time_decode.tostring()
-        self.time_load.tostring()
-        self.time_extract.tostring()
-        self.time_compare.tostring()
-
-        # Remove Lib Files.
-        lib_dir_list = []
-        for i in final_libs_dict:
-            if 'cpn' in final_libs_dict[i]:
-                lib_dir_list.append(apk_path + '/smali/' + final_libs_dict[i]['cpn'])
-        if RM_STATUS > 0:
-            self.rm_lib_files(lib_dir_list)
-
-        zip_file_name = self.project_path + '/../clean_app/' + os.path.basename(apk_path)[:-3] + 'zip'
-        smali_path = apk_path + '/smali'
-        self.zip_apk(smali_path, zip_file_name)
-
-        if RM_STATUS == 1:
-            cmd = 'rm -rf %s' % apk_path
-            subprocess.call(cmd, shell=True)
-        elif RM_STATUS == 2:
-            zip_file_name = self.project_path + '/../clean_app/' + os.path.basename(apk_path)[:-3] + 'zip'
-            smali_path = apk_path + '/smali'
-            self.zip_apk(smali_path, zip_file_name)
-            return zip_file_name
-        return "No Zip File Here."
 
     @staticmethod
     def rm_lib_files(lib_list):
@@ -432,3 +207,230 @@ class Detector:
             bh = (bh + int(a) * this_dict[a]) % 999983          # 99983 is A Big Prime
         self.packages_feature.append((bh, len(this_dict), this_call_num, path, this_permission))
         return this_dict, this_dir_num, this_file_num, this_call_num, this_permission
+
+
+    def get_smali(self, path):
+        """
+        Convert APK into Smali file.
+        :param path:
+        :return: decoded files' path
+        """
+        self.time_decode.start()
+        cmd = self.project_path + "/" + "../tool/apktool decode %s -o " % path + self.project_path + "/" + \
+            "../decoded/%s" % os.path.basename(path)
+        subprocess.call(cmd, shell=True)
+        self.time_decode.end()
+        return self.project_path + '/../decoded/%s' % os.path.basename(path)
+
+    def load_data(self):
+        # - Loading Data
+
+        self.time_load.start()
+        dep_address = self.project_path + "/" + "../data/tgst5.dat"
+        dict_address = self.project_path + "/" + "../permission/tagged_dict.txt"
+        dep_file = open(dep_address, 'r')
+        dict_file = open(dict_address, 'r')
+
+        # -- Loading API Dict
+        for line in dict_file:
+            # print line
+            u = json.loads(line)
+            if 'p' in u:
+                self.api_dict[u['k']] = {'v': u['v'], 'p': u['p']}
+            else:
+                self.api_dict[u['k']] = {'v': u['v'], 'p': []}
+
+        # -- Loading Hashed Libs
+
+        for line in dep_file:
+            # print line
+            u = json.loads(line)
+            """
+            {"dn": 311,                         Repetitions
+             "lib": "pollfish",                 Library
+              "sp": "com/pollfish/f/a",         Simplified Path
+               "bh": 32370,                     B_Hash
+                "btc": 40,                      B_Total_Call
+                 "btn": 12,                     B_Total_Number
+                  "pn": "com/pollfish"}         Package Name
+            """
+            if "pn" in u:
+                components = u['lib'].split(';')
+                assert len(components) == 3
+                lib_type = components[0]
+                if lib_type in self.library_type:
+                    lib_type = self.library_type[lib_type]
+                eng_lib = components[1]        # English Library
+                ch_des = components[2]         # Web Link
+                self.libs_feature.append(
+                    (u['bh'], u['btn'], u['btc'], u['sp'], eng_lib, u['pn'], u['dn'], ch_des, lib_type)
+                )
+            else:
+                self.libs_feature.append((u['bh'],  u['btn'], u['btc'], u['sp'], u['lib'], "", u['dn'], "", ""))
+
+        self.time_load.end()
+
+    def get_hash(self, apk_path):
+        """
+        Convert APK into Smali file.
+        :param path:
+        :return: The path of apk with libs removed.
+        """
+
+        self.time_extract.start()
+
+        # - All Over
+        # print apk_path+'/smali'
+        if os.path.exists(apk_path+'/smali'):
+            os.chdir(apk_path+'/smali')
+            self.all_over(apk_path, '')
+            # os.chdir(apk_path)
+
+        # Print Res
+        # print packages_feature
+        '''
+        if DEBUG:
+            for p in packages_feature:
+                print p
+        '''
+        cur_app_libs = []
+        cur_app_routes = {}
+        path_and_permission = {}
+
+        number_of_tagged_libs = len(self.libs_feature)
+        self.time_extract.end()
+        self.time_compare.start()
+
+        def compare_d(a, b):
+            if a[1] < b[1]:
+                return -1
+            elif a[1] > b[1]:
+                return 1
+            else:
+                if a[2] < b[2]:
+                    return -2
+                elif a[2] > b[2]:
+                    return 2
+                else:
+                    if a[0] < b[0]:
+                        return -3
+                    elif a[0] > b[0]:
+                        return 3
+                    else:
+                        return 0
+
+        def find_feature(package, start, end):
+            if start >= end:
+                return None
+            mid = (start + end) / 2
+            """
+                packages_feature.append((bh, len(this_dict), this_call_num, '/'.join(parts), this_permission))
+            """
+            if compare_d(package, self.libs_feature[mid]) == 0:
+                if self.libs_feature[mid][4] != "" and self.libs_feature[mid][4] != "Nope":
+                    cur_app_libs.append({
+                        "bh": self.libs_feature[mid][0],
+                        "btn": self.libs_feature[mid][1],
+                        "btc": self.libs_feature[mid][2],
+                        "sp": self.libs_feature[mid][3],
+                        "lib": self.libs_feature[mid][4],
+                        "pn": self.libs_feature[mid][5],
+                        "dn": self.libs_feature[mid][6],
+                        "ch": self.libs_feature[mid][7],         # Web Link
+                        "tp": self.libs_feature[mid][8],
+                        "csp": package[3],                  # Current S_path
+                    })
+                elif self.libs_feature[mid][4] == "":
+                    cur_app_routes[self.libs_feature[mid][3]] = {
+                        "pn": self.libs_feature[mid][3],
+                        "cpn": package[3],
+                        "p": package[4],
+                        "dn": self.libs_feature[mid][6]
+                    }
+            elif compare_d(package, self.libs_feature[mid]) < 0:
+                return find_feature(package, mid + 1, end)
+            else:
+                return find_feature(package, start, mid)
+
+        if DEBUG_ON:
+            print "--Packages--"
+
+        def find_features(package):
+            if package[3] != "":
+                if len(package[4]) == 0:
+                    if DEBUG_ON:
+                        print package[3]
+                    path_and_permission[package[3]] = []
+                else:
+                    if DEBUG_ON:
+                        print package[3]+' -- Permission: '+str(package[4])
+                    path_and_permission[package[3]] = package[4]
+            find_feature(package, 0, number_of_tagged_libs)
+
+        for pack in self.packages_feature:
+            if DEBUG_PATH:
+                print ('PACK' + str(pack))
+            find_features(pack)
+        if DEBUG_ON:
+            print "PATH and Permission:"
+            for k in path_and_permission:
+                print k + str(path_and_permission[k])
+        print "--Splitter--"
+        final_libs_dict = {}
+        for i in cur_app_libs:
+            # 先找PN
+            # 然后切分sp。找到对应的path
+            # 然后把对应的Permission找出来加进来
+            if i['pn'] in final_libs_dict:
+                # 找到同样package name的小包中，重复次数最多的，作为总的重复次数
+                # 本来想找到大包的重复次数作为重复次数，但是大包的重复次数甚至很容易直接是0
+                # 并不适合。
+                if final_libs_dict[i['pn']]['dn'] < i['dn']:
+                    final_libs_dict[i['pn']]['dn'] = i['dn']
+            pn_number = len(i['pn'].split('/'))
+            cpn = '/'.join(i['csp'].split('/')[0:pn_number])
+            cpn += '/'
+            i['cpn'] = cpn
+            i['p'] = path_and_permission[cpn]
+            final_libs_dict[i['pn']] = i
+            #print str(i) + ','
+        final_libs_list = []
+        for i in final_libs_dict:
+            final_libs_list.append(final_libs_dict[i])
+        # print json.dumps(final_libs_list)
+        final_routes_list = []
+        for i in cur_app_routes:
+            final_libs_list.append(cur_app_routes[i])
+        print json.dumps(final_libs_list)
+        print "--Splitter--"
+        print "--Splitter--"
+        self.time_compare.end()
+
+        # To String
+        # print "--Time-Consuming--"
+        self.time_decode.tostring()
+        self.time_load.tostring()
+        self.time_extract.tostring()
+        self.time_compare.tostring()
+
+        # Remove Lib Files.
+        lib_dir_list = []
+        for i in final_libs_dict:
+            if 'cpn' in final_libs_dict[i]:
+                lib_dir_list.append(apk_path + '/smali/' + final_libs_dict[i]['cpn'])
+        if RM_STATUS > 0:
+            self.rm_lib_files(lib_dir_list)
+
+        zip_file_name = self.project_path + '/../clean_app/' + os.path.basename(apk_path)[:-3] + 'zip'
+        smali_path = apk_path + '/smali'
+        self.zip_apk(smali_path, zip_file_name)
+
+        if RM_STATUS == 1:
+            cmd = 'rm -rf %s' % apk_path
+            subprocess.call(cmd, shell=True)
+        elif RM_STATUS == 2:
+            zip_file_name = self.project_path + '/../clean_app/' + os.path.basename(apk_path)[:-3] + 'zip'
+            smali_path = apk_path + '/smali'
+            self.zip_apk(smali_path, zip_file_name)
+            return zip_file_name
+        return "No Zip File Here."
