2c6e7e299185a2b9bb43aff4f238b697f0497d7e
pkumza <pkumza@163.com>
2016-12-10 13:12:22 +0800

    import redis. Get redis support and try to walk the directory in next step.

--- LibRadarData/API_Dict.py
+++ LibRadarData/API_Dict.py
@@ -10,6 +10,22 @@
 
 import os
 import commands
+import redis
+
+#TODO: Databases were made for? To be completed.
+"""
+    Information about Database.
+    Redis is used here and the default setting points to localhost:6379
+    If your Redis IP and Port is different, you can change the value here.
+    In my redis.conf, I set 32 for the maximum number of databases.
+    What's the databases for?
+
+        0 : Android.jar Classes
+
+"""
+db_host = 'localhost'
+db_port = 6379
+db_id = 0
 
 """
     Implementation for Singleton
@@ -28,6 +44,7 @@ class API_Dictionary_Generator(Singleton):
 
     def __init__(self):
         self.jar_list = []
+        self.redis_database = redis.StrictRedis(host=db_host, port=db_port, db=db_id)
 
     """
         Test if jad command exists.
@@ -84,18 +101,30 @@ class API_Dictionary_Generator(Singleton):
 
     """
         Walk through the directories to find all the class names and methods (SDK API).
+
+
     """
     def walk_dir(self):
         for jar in self.jar_list:
             dir_2B_walked = "../Data/RawData/" + jar + ".dir"
             print("Walk:")
+            # TODO: turn java source into tree.
+            # That's simple, try to identify public type method(type),
+            # 找到 函数的特点 类的特点 内部类的特点 以及构造函数的特点即可
+
+            # TODO: Redis 的 INCR 是线程安全的，所以可以尝试多线程来完成这一步的操作。
             for dirName, subdirList, fileList in os.walk(dir_2B_walked):
                 print("Directory %s" % dirName)
-                for fname in fileList:
-                    print("\t%s" % fname)
+                for filename in fileList:
+                    if len(filename) > 4 and filename[-5:] == ".java":
+                        full_path_name = '/'.join(dirName.split('/')[4:]) + '/' + filename
+                        print("\t%s" % full_path_name)
+                        class_name = full_path_name[:-5].replace('/','.')
+                        self.redis_database.incr(class_name)
 
 
 adg = API_Dictionary_Generator()
 adg.add_jar('android24.jar')
-adg.decompile_jar()
+'''decompiling is only needed once.'''
+# adg.decompile_jar()
 adg.walk_dir()
