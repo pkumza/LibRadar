d2c976ba9baf3fd4214fd1d971ad6ca91365bdfe
LabMonkey <pkumza@163.com>
2017-02-03 21:02:51 +0800

    Fix bugs and delete unused function.
    1. 多一层搜索|one degree deeper
    2. 解决match的similarity溢出|Fix similarity overflow bug
    3. slightly change of rules, remove repeated gson.
    4. 删除tree.py里面的一些检测信息

--- LibRadar/dex_tree.py
+++ LibRadar/dex_tree.py
@@ -9,7 +9,6 @@
 """
 
 from _settings import *
-import binascii
 import hashlib
 import csv
 import redis
@@ -46,7 +45,6 @@ class TreeNode(object):
     }
     """
     def __init__(self, n_weight=-1, n_pn="", n_parent=None):
-        # TODO exam : import binascii hexlify
         self.md5 = ""
         self.weight = n_weight
         self.pn = n_pn
@@ -68,24 +66,6 @@ class TreeNode(object):
             self.children[target_package_name] = TreeNode(n_weight=weight, n_pn=target_package_name, n_parent=self)
             return self.children[target_package_name].insert(package_name, weight, md5)
 
-    def get_node(self, package_name):
-        current_depth = 0 if self.pn == "" else self.pn.count('/') + 1
-        target_depth = package_name.count('/') + 1
-        if current_depth == target_depth:
-            return self
-        target_package_name = '/'.join(package_name.split('/')[:current_depth + 1])
-        if target_package_name in self.children:
-            return self.children[target_package_name].get_node(package_name)
-        else:
-            return None
-
-    def to_string(self):
-        print("---- TreeNode toString ----")
-        print("md5:%s" % binascii.hexlify(self.md5))
-        print("weight:%d" % self.weight)
-        print("pn:%s" % self.pn)
-        print("---- print parent Later----")
-
 
 class Tree(object):
     """
@@ -97,20 +77,10 @@ class Tree(object):
     def insert(self, package_name, weight, md5):
         self.root.insert(package_name, weight, md5)
 
-    def get_node(self, package_name):
-        """
-        Used for update.
-        :param package_name:
-        :return:
-        """
-        return self.root.get_node(package_name)
-
     def pre_order(self, visit):
         self._pre_order(self.root, visit)
 
     def _pre_order(self, node, visit):
-        if node is None:
-            return
         ret = visit(node)
         if ret < 0:
             return
@@ -122,30 +92,15 @@ class Tree(object):
         self._post_order(self.root, visit)
 
     def _post_order(self, node, visit):
-        if node is None:
-            return
         for child_pn in node.children:
             self._post_order(node.children[child_pn], visit)
         visit(node)
-
-    def print_node(self, package_name):
-        node = self.get_node(package_name)
-        if isinstance(node, TreeNode):
-            node.to_string()
-        else:
-            logger.error("Wrong node to be printed.")
 
     @staticmethod
     def _cal_md5(node):
         # Ignore Leaf Node
         if len(node.children) == 0 and node.md5 != "":
             return
-        # Test bug: Wrong Node
-        if len(node.children) == 0:
-            logger.error("Md5 is not empty but no child")
-        # Test bug: Wrong Node
-        if node.md5 != "":
-            logger.error("Children exist but md5 is not empty")
         # Everything seems Okay.
         cur_md5 = hashlib.md5()
         md5_list = list()
@@ -167,7 +122,6 @@ class Tree(object):
     @staticmethod
     def _match(node):
         a = db_un_ob_pn.get(node.md5)
-        w = db_feature_weight.get(node.md5)
         c = db_feature_count.get(node.md5)
         u = db_un_ob_pn_count.get(node.md5)
         """ Debug Log
@@ -181,18 +135,16 @@ class Tree(object):
         # if could not find this package in database, search its children.
         if a is None:
             return 1
-        # CRITICAL PROBLEM
-        if int(w) != node.weight:
-            logger.critical("Same Md5, Diff Weight: %s" % (binascii.hexlify(node.md5)))
         # Potential Name is not convincing enough.
         if u < 8 or float(u) / float(c) < 0.3:
             return 2
+        flag_not_deeper = False
         for lib in labeled_libs:
             # if the potential package name is the same as full lib path
             # do not search its children
             if lib[0] == a:
                 node.match.append([lib, node.weight])
-                return -1
+                continue
             # If they have the same length but not equal to each other, just continue
             if len(lib[0]) == len(a):
                 continue
@@ -200,14 +152,14 @@ class Tree(object):
             #   e.g. a is Lcom/google, we could find it as a part of Lcom/google/android/gms, so search its children for
             #       more details
             if len(a) < len(lib[0]) and a == lib[0][:len(a)]:
-                return 3
+                continue
             # If the lib path is part of potential package name, add some count into parent's match list.
             if len(a) > len(lib[0]) and lib[0] == a[:len(lib[0])]:
                 depth_diff = a.count('/') - lib[0].count('/')
                 cursor = node
                 for i in range(depth_diff):
                     # cursor should not be the root, so cursor's parent should not be None.
-                    if cursor.parent.parent != None:
+                    if cursor.parent.parent is not None:
                         cursor = cursor.parent
                     else:
                         # root's parent is None
@@ -219,10 +171,69 @@ class Tree(object):
                 for matc in cursor.match:
                     # if matc[0][0] == lib[0]:
                     if matc[0] == lib:
-                        matc[1] += node.weight
                         flag = True
+                        if matc[1] != cursor.weight:
+                            matc[1] += node.weight
                 if not flag:
                     cursor.match.append([lib, node.weight])
+                flag_not_deeper = True
+                continue
+        """
+            One degree deeper!
+            深入探测一层
+
+                There's a situation that a package is a library and the child of a package is also a library.
+                库是存在相互嵌套的。
+
+                As we all know that Lcom/unity3d is definitely a Game Engine library. There could be some sub-package
+                like Lcom/unity3d/player, Lcom/unity3d/plugin, Lcom/unity3d/sdk, etc. So we take Lcom/unity3d as the
+                root package of this library.
+                比如，Lcom/unity3d 显然是Unity3D这个游戏引擎，在游戏引擎下可能会有player, plugin, sdk等次级包（文件夹），所以我们很
+                显然地把Lcom/unity3d作为游戏引擎的根包。
+
+                However, Lcom/unity3d/ads is an Advertisement library.
+                但是，Lcom/unity3d/ads是Unity3D公司推出的广告库
+
+                If we do not search one degree deeper, we could only find the game engine other than the ads library.
+                Likewise, we could not find Landroid/support/v4 anymore if we take Landroid/support as a library.
+                如果我们不继续搜索的话，那么对于一个应用，我们只能检测到Unity3D这个引擎，无法检测到Unity3D Ads这个广告库。
+
+            Implementation:
+            实现：
+                if lib[0] == a, we continue search his children.
+                if lib[0] == a 这个后面从return变成了continue，我们会继续搜索它的子节点
+
+                if we already found his child, we will not search deeper.
+                在后面的代码中，如果已经知道的就是子节点，那么就不会继续深层的搜了。
+
+                In my original code, I found a bug that the match degree is larger than the total amount of weight.
+                This is impossible. After debugging, I found that if I add the match value multiple times, the match
+                weight could overflow.
+                在我原来有bug的代码中，我发现匹配的similarity有大于1的情况，即com/facebook这个库的similarity大于了1。这是因为match
+                被我加总了数次
+
+                For example:
+                    There's a library Lcom/google/android/gson, weight is 189
+                    we found Lcom/google/android/gson, so add the weight 189
+                    we found Lcom/google/android/gson/internal, so add the weight 24
+                    we found Lcom/google/android/gson/stream, so add the weight 43
+                    In this case, the weight of package gson overflows.
+                举例来看：
+                    对于Lcom/google/android/gson这个包来说，它的API数量是189
+                    搜索中找到 Lcom/google/android/gson， weight加上189
+                    搜索中找到 Lcom/google/android/gson/internal， weight加上24
+                    搜索中找到 Lcom/google/android/gson/stream, weight加上 43
+                    这样显然就溢出了。
+
+                Because we only search 1 degree deeper, the match situation of Lcom/google/android/gson is only true or
+                false. In this case, we just need to check if the weight has overflowed before add weight. as the code:
+                    if matc[1] != cursor.weight:
+                        matc[1] += node.weight
+                因为我们可以多搜一层，所以判断是否溢出很简单。因为对于上层的库来说，也就只有两种情况，那就是匹配到和没匹配到。所以只需要
+                检测一下是否已经超出就行了。
+        """
+        if flag_not_deeper:
+            return -1
         # Never find a good match, search its children.
         return 5
 
@@ -245,32 +256,3 @@ class Tree(object):
         print("\n===== RESULT: ============")
         self.pre_order(visit=self._get_lib)
         print("==========================")
-
-
-if __name__ == '__main__':
-    print " === Test starts ===="
-    pass
-    """
-    tn = Tree()
-    tn.insert("a/b", 12, "")
-    tn.insert("a/c", 14, "")
-    tn.insert("b/a/s", 16, "")
-    tn.insert("b/h", 16, "")
-    tn.insert("c/b", 414, "")
-    tn.insert("b/a/e", 16, "")
-    tn.insert("b/e", 16, "")
-    tn.insert("b/a/s/cse", 16, "")
-    tn.insert("b/a/s/cst", 16, "")
-    tn.insert("b/a/s/csa", 16, "")
-    def vis(node):
-        if node.parent is None:
-            pass
-        else:
-            print "L: " + node.pn + " P: " + node.parent.pn
-        print node.weight
-        return 0
-    tn.post_order(vis)
-    print "CS:"
-    print tn.get_node("b/a/s").pn
-    """
-    print " === Test ends   ===="
