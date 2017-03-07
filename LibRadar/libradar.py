188eea22e76200bc94af99a6dcb7d3a7ff94cbcf
labsnail <pkumza@163.com>
2017-03-07 10:21:27 +0800

    libradar main arguments

--- LibRadar/libradar.py
+++ LibRadar/libradar.py
@@ -214,6 +214,11 @@ class LibRadar(object):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("LibRadar only takes 1 arguments.")
+        print("Usage:")
+        print("    $ python libradar.py example.apk")
+        exit(1)
     apk_path = sys.argv[1]
     lrd = LibRadar(apk_path)
     res = lrd.compare()
