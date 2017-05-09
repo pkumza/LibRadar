4f5973e4526889a94b3319e6b909b331f102b90f
LabMonkey <pkumza@163.com>
2017-05-09 12:30:13 +0800

    bug fix

--- LibRadar/dex_extracting.py
+++ LibRadar/dex_extracting.py
@@ -150,7 +150,7 @@ class PackageNodeList:
     """
     def __init__(self):
         self.pn_list = list()
-        self.db = redis.StrictRedis(host=DB_HOST, port=DB_PORT, db=DB_ID, password=DB_PSWD)
+        self.db = redis.StrictRedis(host=DB_HOST, port=DB_PORT, db=2, password=DB_PSWD)
 
     def flush_db(self):
         """
