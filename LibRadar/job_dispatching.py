45bc5bb3eb49ed1714e73e6b97ac21c449b602f6
LabMonkey <pkumza@163.com>
2017-05-09 12:22:29 +0800

    bug fix test

--- LibRadar/job_dispatching.py
+++ LibRadar/job_dispatching.py
@@ -39,7 +39,7 @@ except:
 
 while True:
     try:
-        item = db.blpop(name="apk_new_2").split(" ")
+        item = db.blpop("apk_new_2")[1].split(" ")
         assert(len(item) == 5)
     except:
         logging.error("item get error!")
