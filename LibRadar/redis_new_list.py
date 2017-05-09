8832e0600befb42f50bc9882e234d7f35e144491
LabMonkey <pkumza@163.com>
2017-05-09 17:32:56 +0800

    create apk_new_3 list

--- LibRadar/redis_new_list.py
+++ LibRadar/redis_new_list.py
@@ -4,16 +4,11 @@ import logging
 
 db = redis.StrictRedis(host="redis.pkuos.org", port=DB_PORT, db=0, password=DB_PSWD)
 
-
-
-for i in range(2119):
+for i in range(2167):
     a = db.lrange(name="apk_queue", start=-1000-i*1000, end=-1-i*1000)
     multi = db.pipeline(transaction=False)
     for j in a:
-        if j.startswith("New "):
-            item = j.split(" ")
-            key = '/'.join(item[1:-1]) + '-' + item[-1] + ".apk"
-            if db.sismember("processed_2", key):
-                multi.rpush("apk_new_2", j)
+        if j.startswith("New ") or j.startswith("UpdateVersion "):
+            multi.rpush("apk_new_3", j)
     multi.execute()
     logging.info("%d", i)
